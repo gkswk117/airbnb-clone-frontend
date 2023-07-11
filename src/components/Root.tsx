2f708dc5ba5bbcc15adee56a544301b5a9d31880
Hanwoong <gkswk117@gmail.com>
2023-07-12 02:32:49 +0900

    [풀스택 에어비앤비 클론코딩] #18.0 Header (13:47) 완료.

--- src/components/Root.tsx
+++ src/components/Root.tsx
@@ -1,10 +1,21 @@
+import { Box, Button, HStack } from "@chakra-ui/react";
 import { Outlet } from "react-router-dom";
+import {FaAirbnb} from "react-icons/fa";
 
 export default function Root(){
     return (
-        <h1>
-            I'm Root.
+        <Box>
+            <HStack justifyContent={"space-between"} py={10} px={5} borderBottomWidth={1}>
+                <Box color={"red.500"}>
+                    <FaAirbnb size={"48px"}/>
+                </Box>
+                {/*Box로 감싼 이유: FaAirbnb는 Chakra UI가 아니다. 그래서 red.500값을 못쓰고, #E53E3E값을 직접 입력해줘야 한다.*/}
+                <HStack spacing={2}>
+                    <Button>Sign In</Button>
+                    <Button>Sign Up</Button>
+                </HStack>
+            </HStack>
             <Outlet/>
-        </h1>
+        </Box>
     )
 }
