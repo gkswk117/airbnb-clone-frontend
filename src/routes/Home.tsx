deafaa5250cd9a8aba491f271d106dae0b61277f
Hanwoong <gkswk117@gmail.com>
2023-07-16 21:53:03 +0900

    [풀스택 에어비앤비 클론코딩] #18.4 Rooms Grid (16:07) [풀스택 에어비앤비 클론코딩] #18.5 Responsive Design (14:25) 완료.

--- src/routes/Home.tsx
+++ src/routes/Home.tsx
@@ -1,3 +1,31 @@
-export default function Home(){
-    return <span>It's Home.</span>
+import { Grid } from "@chakra-ui/react";
+import Room from "../components/Room";
+
+export default function Home() {
+  //"repeat(6, 1fr)" === "1fr 1fr 1fr 1fr 1fr 1fr"
+  return (
+    <Grid
+      mt={10}
+      px={{
+        base: 10,
+        lg: 40,
+      }}
+      columnGap={4}
+      rowGap={8}
+      templateColumns={{
+        sm: "1fr",
+        md: "1fr 1fr",
+        lg: "repeat(3, 1fr)",
+        xl: "repeat(4, 1fr)",
+        "2xl": "repeat(5, 1fr)",
+      }}
+    >
+      {[
+        1, 2, 3, 4, 56, 7, 7, 7, 6, 6, 6, 6, 66, 6, 6, 6, 6, 2, 2, 2, 2, 2, 2,
+        2, 2, 2, 2,
+      ].map((index) => (
+        <Room key={index} />
+      ))}
+    </Grid>
+  );
 }
