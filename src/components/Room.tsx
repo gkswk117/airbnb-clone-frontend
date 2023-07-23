87ec2abad0f140b9e3422823283aecc48adf96ed
Hanwoong <gkswk117@gmail.com>
2023-07-24 04:29:15 +0900

    [풀스택 에어비앤비 클론코딩] #19.0 Manual Fetching (14:35) 완료.

--- src/components/Room.tsx
+++ src/components/Room.tsx
@@ -9,14 +9,20 @@ import {
   useColorModeValue,
   VStack,
 } from "@chakra-ui/react";
-
-export default function Room() {
+interface roomType{
+  name:string,
+  city:string,
+  country:string,
+  rating:number,
+  price:number
+}
+export default function Room(props:roomType) {
   const gray = useColorModeValue("gray.600", "gray.300");
   return (
     <VStack alignItems={"flex-start"}>
       <Box position="relative" overflow={"hidden"} mb={3} rounded="2xl">
         <Image
-          minH="280"
+          minH="100"
           src="https://a0.muscache.com/im/pictures/miso/Hosting-47181423/original/39c9d4e7-78d0-4807-9f0d-3029d987d02a.jpeg?im_w=720"
         />
         <Button
@@ -32,19 +38,19 @@ export default function Room() {
       <Box>
         <Grid gap={2} templateColumns={"6fr 1fr"}>
           <Text display={"block"} as="b" noOfLines={1} fontSize="md">
-            Cheomdangwahak-ro,Jeongeup-si, North Jeolla Province, South Korea
+            {props.name}
           </Text>
           <HStack spacing={1} alignItems="center">
             <FaStar size={12} />
-            <Text fontSize={"sm"}>5.0</Text>
+            <Text fontSize={"sm"}>{props.rating}</Text>
           </HStack>
         </Grid>
         <Text fontSize={"sm"} color={gray}>
-          Seoul, S. Korea
+        {props.city} {props.country}
         </Text>
       </Box>
       <Text fontSize={"sm"} color={gray}>
-        <Text as="b">$72</Text> / night
+        <Text as="b">{props.price}</Text> / one night
       </Text>
     </VStack>
   );
--- src/routes/Home.tsx
+++ src/routes/Home.tsx
@@ -1,7 +1,25 @@
 import { Grid } from "@chakra-ui/react";
 import Room from "../components/Room";
+import { useEffect, useState } from "react";
+import RoomSkeleton from "../components/RoomSkeleton"
 
 export default function Home() {
+  const [isLoading, setIsLoading] = useState(true)
+  const [rooms, setRooms] = useState([])
+  async function fetchRooms(){
+    const response = await fetch("http://127.0.0.1:8000/api/v1/rooms/")
+    const json = await response.json()
+    setRooms(json)
+    setIsLoading(false)
+  }
+  useEffect(()=>{
+    fetchRooms()
+
+  }, [])
+  useEffect(()=>{
+    console.log("rooms is ")
+    console.log(rooms)
+  },[rooms])
   //"repeat(6, 1fr)" === "1fr 1fr 1fr 1fr 1fr 1fr"
   return (
     <Grid
@@ -20,12 +38,9 @@ export default function Home() {
         "2xl": "repeat(5, 1fr)",
       }}
     >
-      {[
-        1, 2, 3, 4, 56, 7, 7, 7, 6, 6, 6, 6, 66, 6, 6, 6, 6, 2, 2, 2, 2, 2, 2,
-        2, 2, 2, 2,
-      ].map((index) => (
-        <Room key={index} />
-      ))}
+      {isLoading ? <><RoomSkeleton/><RoomSkeleton/><RoomSkeleton/><RoomSkeleton/><RoomSkeleton/><RoomSkeleton/><RoomSkeleton/><RoomSkeleton/><RoomSkeleton/><RoomSkeleton/></>
+      :rooms.map((room)=><Room name={room["name"]} city={room["city"]} country={room["country"]} rating={room["rating"]==="리뷰 없음."?0:room["rating"]} price={room["price"]}/>)}
+      
     </Grid>
   );
 }
