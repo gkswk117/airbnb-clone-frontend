8aca41be5ac166ba7f834d7d4b44d2293b163288
Hanwoong <gkswk117@gmail.com>
2023-08-01 15:16:50 +0900

    [풀스택 에어비앤비 클론코딩] #19.3 Axios (06:09) 완료.

--- src/api.ts
+++ src/api.ts
@@ -1,6 +1,13 @@
+import axios from "axios"
+/* noob
 const BASE_URL = "http://127.0.0.1:8000/api/v1"
 export async function getRooms(){
   const response = await fetch(`${BASE_URL}/rooms/`)
   const json = await response.json()
   return json
-}
+*/
+// pro
+const axiosInstance = axios.create({
+  baseURL:"http://127.0.0.1:8000/api/v1"
+})
+export const getRooms = ()=> axiosInstance.get("rooms/").then(response=>response.data)
