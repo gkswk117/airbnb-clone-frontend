2dd5fdf10920dfe78e36ae8aee318b40b894fa2f
Hanwoong <gkswk117@gmail.com>
2023-08-07 20:44:24 +0900

    [풀스택 에어비앤비 클론코딩] #20.5 Github Log In (11:25) 완료.

--- src/components/SocialLogin.tsx
+++ src/components/SocialLogin.tsx
@@ -1,9 +1,9 @@
 import { Box, Button, Divider, HStack, Text, VStack } from "@chakra-ui/react";
-import { FaComment, FaGithub} from "react-icons/fa";
+import { FaComment, FaGithub } from "react-icons/fa";
 
-export default function SocialLogin(){
-    return (
-      <Box mb={4}>
+export default function SocialLogin() {
+  return (
+    <Box mb={4}>
       <HStack my={8}>
         <Divider />
         <Text textTransform={"uppercase"} color="gray.500" fontSize="xs" as="b">
@@ -12,7 +12,13 @@ export default function SocialLogin(){
         <Divider />
       </HStack>
       <VStack>
-        <Button w="100%" leftIcon={<FaGithub />} colorScheme={"githubButton"}>
+        <Button
+          as="a"
+          href="https://github.com/login/oauth/authorize?client_id=ba5320afbf14928eed15&scope=read:user,user:email"
+          w="100%"
+          leftIcon={<FaGithub />}
+          colorScheme={"githubButton"}
+        >
           Continue with Github
         </Button>
         <Button w="100%" leftIcon={<FaComment />} colorScheme={"yellow"}>
@@ -20,5 +26,5 @@ export default function SocialLogin(){
         </Button>
       </VStack>
     </Box>
-  )
+  );
 }
